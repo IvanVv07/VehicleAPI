c1c52cd4732f59f9246426e50fe7793960838cfe
Ivan <a2021116557@estudiantes.upsa.edu.bo>
2025-09-01 14:33:39 -0400

    "Colocamos el codigo adentro del Vehicle.cs"

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,6 +1,14 @@
-﻿namespace VeehicleeAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeehicleeAPI.Models;
 
 public class Vehicle
 {
-    
+  public Guid Id { get; init; } = Guid.NewGuid();
+  [Required, StringLength(50)]
+  public string Make { get; init; } = null!;
+  [Required, StringLength(50)]
+  public string Model { get; init; } = null!;
+  [Range(1900, 2100)]
+  public int Year { get; init; }
 }
