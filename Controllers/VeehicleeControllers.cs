ae48911e6417f6271b4e416a835c24f7f39d3def
Ivan <a2021116557@estudiantes.upsa.edu.bo>
2025-09-01 15:10:08 -0400

    el codigo para el controllador

--- Controllers/VeehicleeControllers.cs
+++ Controllers/VeehicleeControllers.cs
@@ -1,23 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace VeehicleeAPI.Controllers  // 👈 usa tu namespace exacto
-{
-    [ApiController]
-    [Route("api/[controller]")]   // quedará: api/vehicles
-    public class VehiclesController : ControllerBase
-    {
-        // GET api/vehicles
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new[] { "value1", "value2" };
-        }
+namespace VeehicleeAPI.Controllers;
 
-        // GET api/vehicles/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+[Route(template: "api/[controller]")]
+[ApiController]
+public class VehiclesController : ControllerBase
+{
+    // GET: api/<VehiclesController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET: api/<VehiclesController>/5
+    [HttpGet(template: "{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST: api/<VehiclesController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT: api/<VehiclesController>/5
+    [HttpPut(template: "{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE: api/<VehiclesController>/5
+    [HttpDelete(template: "{id}")]
+    public void Delete(int id)
+    {
     }
 }
